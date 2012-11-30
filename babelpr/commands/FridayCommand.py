f2913f444117279ef9aaf79b4bf47c3f0e57d171
Craig Citro <craig.citro@abt.com>
2012-11-30 09:28:10 -0600

    Add 'friday' command

---
+++ babelpr/commands/FridayCommand.py
@@ -0,0 +1,5 @@
+from babelpr.commands import RandomResponseCommand
+
+class FridayCommand(RandomResponseCommand):
+    triggers = ['friday']
+    _responses = ['FRIDAY FRIDAY, GOTTA GET DOWN ON FRIDAY']
