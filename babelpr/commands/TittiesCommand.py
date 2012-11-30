3ce6e26f74180abf41a92876f171cf62bc865b27
Craig Citro <craig.citro@abt.com>
2012-11-30 12:39:53 -0600

    Added titties command

---
+++ babelpr/commands/TittiesCommand.py
@@ -0,0 +1,5 @@
+from babelpr.commands import RandomResponseCommand
+
+class TittiesCommand(RandomResponseCommand):
+    triggers = ['titties']
+    _responses = ["Syntax error: when refering to 'the titties command', command is a verb"]
