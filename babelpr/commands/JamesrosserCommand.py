680b5b4492ad4f7091dc23a9bdb4e8dde17fd276
Craig Citro <craig.citro@abt.com>
2012-11-30 09:22:51 -0600

    Add JamesrosserCommand

---
+++ babelpr/commands/JamesrosserCommand.py
@@ -0,0 +1,5 @@
+from babelpr.commands import RandomResponseCommand
+
+class JamesrosserCommand(RandomResponseCommand):
+    triggers = ['jamesrosser']
+    _responses = ['8=====================================================================================================================================================================================================D']
