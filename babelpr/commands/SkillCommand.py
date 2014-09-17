e49b030c53dcac7fbe8f1d2aea229f72b9d1b096
Craig Citro <craig.citro@abt.com>
2014-09-17 12:18:15 -0500

    Update skill command to pull from riot

--- babelpr/commands/SkillCommand.py
+++ babelpr/commands/SkillCommand.py
@@ -18,12 +18,11 @@ class SkillCommand(ExplicitCommand):
         self.syntax = "#skill CHAMPION,{PASSIVE,Q,W,E,R}"
         
         self.skill_map = {
-          'PASSIVE': 'skill_innate',
-          'INNATE': 'skill_innate',
-          'Q': 'skill_q',
-          'W': 'skill_w',
-          'E': 'skill_e',
-          'R': 'skill_r'
+          'PASSIVE': 0,
+          'Q': 1,
+          'W': 2,
+          'E': 3,
+          'R': 4
         }
         
 
@@ -39,44 +38,46 @@ class SkillCommand(ExplicitCommand):
         skill = arg_parts[1].strip().upper()
         if not skill in self.skill_map:
             return "Unknown skill label '%s'.  Please specify passive, Q, W, E, or R" % skill
-        class_name = self.skill_map[skill]
+        skill_seq = self.skill_map[skill]
         
-        champion_name = self.getWikiaChampName(arg_parts[0].strip())
+        champion_name = self.formatChampName(arg_parts[0].strip())
         if champion_name is None :
             return "Unknown champion: " + arg_parts[0] 
         
         
         
-        url = "http://leagueoflegends.wikia.com/wiki/"+champion_name
+        url = "http://gameinfo.na.leagueoflegends.com/en/game-info/champions/"+champion_name+'/'
         data = getWebpage(url)
         soup = BeautifulSoup(data)
         
-        champname_span = soup.select('#champion_info span:first')
+        champname_node = soup.select('#champ_header h1')
         
-        if not champname_span or len(champname_span) == 0:
+        if not champname_node or len(champname_node) == 0:
             return "Sorry, I couldn't find info for champion '%s'" % champion_name
         
-        formatted_champ_name = champname_span[0].string
+        formatted_champ_name = champname_node[0].string
         
-        skill_div = soup.select('div.'+class_name)
-        if not skill_div or len(skill_div) == 0:
+        passive_div = soup.select('#spell-passive')
+        if not passive_div or len(passive_div) == 0:
             return "Sorry, I couldn't find info on skill '%s' for %s" % (skill, formatted_champ_name)
-        
-        skill_soup = skill_div[0]
+
+        skills_container = passive_div[0].parent.parent
+        skill_nodes = skills_container.select(".section-wrapper-content-wrapper > div.gs-container")
+
+        if not skill_nodes or len(skill_nodes) <= skill_seq:
+            return "Sorry, I couldn't parse the info on skill '%s' for %s" % (skill, formatted_champ_name)
         
         try:
-            skill_name = self.requiredSelect(skill_soup, '.skill_header > span').string
-            cooldown_cost = self.flatten(self.requiredSelect(skill_soup, '.skill_header > ul'))
-            cooldown_cost = re.sub("(\s)(\w+):", r',\1\2', cooldown_cost)
-            
-            sub_section = skill_soup.select('.skill_wrapper > div')[1]
-            skill_text = self.flatten(self.requiredSelect(sub_section, 'div'))
-            skill_leveling = self.flatten(self.requiredSelect(sub_section, 'div.skill_leveling'))
+            skill_node = skill_nodes[skill_seq]
+            skill_name = self.requiredSelect(skill_node, 'h3').string
+            skill_des = self.flattenRequiredSelect(skill_node, 'p')
+            skill_des = skill_des.replace("Range: ", " Range: ")
+            skill_des = re.sub(r'\s{2,}', '\n', skill_des)
         except:
             return "Sorry, I couldn't parse the info on skill '%s' for %s" % (skill, formatted_champ_name)
             
         
-        ret = "%s (%s) for %s:\n%s\n%s\n%s" % (skill_name, skill, formatted_champ_name, cooldown_cost, skill_text, skill_leveling) 
+        ret = "%s (%s) for %s:\n%s" % (skill_name, skill, formatted_champ_name, skill_des) 
         re.sub("\n\n+" , "\n", ret)
         return ret
     
@@ -91,6 +92,16 @@ class SkillCommand(ExplicitCommand):
         if not match or len(match) == 0:
             raise Exception("requried selector not found: " + selector)
         return match[0]
-    
-    def getWikiaChampName(self, champ_name):
-        return champ_name
+
+    def flattenRequiredSelect(self, soup, selector):
+        matches = soup.select(selector)
+        if not matches or len(matches) == 0:
+            raise Exception("requried selector not found: " + selector)
+        
+        text = ""
+        for match in matches:
+            text += self.flatten(match) + " "
+        return text
+
+    def formatChampName(self, champ_name):
+        return champ_name.lower()
